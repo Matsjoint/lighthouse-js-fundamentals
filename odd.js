0e1e721cdbf00e3dc0d73c1a772b7fc20634b946
matsjoint <you@example.commatsjoint@gmail.com>
2020-08-26 21:37:46 +0000

    function that checks if a number is odd

---
+++ odd.js
@@ -0,0 +1,13 @@
+const isOdd  = function (num) {
+  return num % 2 === 1;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+/*
+
+Original attempt: tried to just replace the 2 from isEven with 1 but it said both numbers were true (3, 8)
+Second attempt: replaced the remainder of '0' with '1'
+
+*/
