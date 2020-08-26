bf832ecfeb814b59c3d7ad9a9bbfd71f8adcea29
matsjoint <you@example.commatsjoint@gmail.com>
2020-08-26 20:40:07 +0000

    Using parameters in functions

---
+++ hello.js
@@ -0,0 +1,16 @@
+/* const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+*/
+// 2nd code blox Ex.
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
