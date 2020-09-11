4e112ed03440e7a5dc84a2914148ef2281051684
matsjoint <matsjoint@gmail.com>
2020-09-11 21:03:27 +0000

    For vs While Loops - Arrays

---
+++ ice-cream.js
@@ -0,0 +1,21 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+// a.
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// b. Get First Flavor
+
+console.log(iceCreamFlavours[0]);
+
+// c. Get the Last Flavour
+
+console.log(iceCreamFlavours[5]);
+
+// d. Total Number of Flavors
+
+console.log(iceCreamFlavours.length);
