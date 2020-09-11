7a0b966d9939e6ea77b805a3c8ec65cd01178c39
matsjoint <matsjoint@gmail.com>
2020-09-11 21:07:42 +0000

    while loop revision

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,6 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
-// 'i' is a counter variable.  
-// Loop below increments i var (index variable)
-
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
